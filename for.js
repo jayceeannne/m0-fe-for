53fa7aec66d411aea3d7a7b8de6dd68d92956581
jayceeannne <136057571+jayceeannne@users.noreply.github.com>
2023-06-27 21:24:42 -0700

    save revisions

--- for.js
+++ for.js
@@ -3,35 +3,37 @@
   
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
-var evenNumbers = ["26", "28", "30", "32", "70", "72", "74"]
+var evenNum = ["26", "28", "30", "32", "70", "72", "74"]
 
-for (var i = 26; i <= 74; i + 2) {
+  for (var i = 26; i <= 74; i++) {
+  if (i % 2 === 0){
   console.log(i);
+}
 }
  
 
-// Challenge 2
-// Use a for statement to log to the console a complete sentence stating the weather using the Array below:
+// // Challenge 2
+// // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
-for (var i = 0; i < weather_options; i++) {
-  console.log("The weather will be " + weather_options[i]);
+for (var i = 0; i < weather_options.length; i++) {
+  console.log(`The weather will be ${weather_options[i]}`); 
 }
 
 
 
-// Challenge 3
-// Use a for statement to log to the console only the words that are exactly 3 characters in length
+// // Challenge 3
+// // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
-for (var i = 0; i < words.lenth; i++) {
-  if (words[i].lenght === 3) {
+for (var i = 0; i < words.length; i++) {
+  if (words[i].length === 3) {
     console.log(words[i]);
   }
 }
 
-// 📹 Record yourself live-coding and talking through any ONE of the challenges. 
-  // The recording doesn't have to be the first time you are solving the problem. 
-  // Use technical vocabulary and explain each part to demonstrate your understanding! 
-  // Share the link to your video in your small group channel.
+// // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
+//   // The recording doesn't have to be the first time you are solving the problem. 
+//   // Use technical vocabulary and explain each part to demonstrate your understanding! 
+//   // Share the link to your video in your small group channel.
 
